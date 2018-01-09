8264b20489e8d464e0f267d552e5b500d3e3f3b5
Jack Scott <mast.j.scott@gmail.com>
2018-01-09 15:51:27 +0800

    connected all the reducers and added raven #3

--- src/store.js
+++ src/store.js
@@ -1,11 +1,31 @@
 import { createStore, applyMiddleware, combineReducers } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
+import Raven from 'raven-js';
+import createRavenMiddleware from 'raven-for-redux';
 import thunkMiddleware from 'redux-thunk';
+import { reducer as form } from 'redux-form';
+import player from './player/player.reducer';
+import workspace from './workspace/workspace.reducer';
+import template from './template/template.reducer';
+import chronicle from './chronicle/chronicle.reducer';
+import render from './render/render.reducer';
+import membership from './membership/membership.reducer';
+import config from './config';
+
+Raven.config(config.ravenDSN).install();
 
 const reducers = {
+  form,
+  player,
+  workspace,
+  template,
+  chronicle,
+  render,
+  membership,
 };
 
 const middleware = [
+  createRavenMiddleware(Raven), // must be first
   thunkMiddleware,
 ];
 
