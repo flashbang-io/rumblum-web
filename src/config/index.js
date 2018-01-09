cd02ce5921bc939db07e2debb8009df2931a6b35
Jack Scott <mast.j.scott@gmail.com>
2018-01-09 13:59:37 +0800

    added configuration files #1

---
+++ src/config/index.js
@@ -0,0 +1,10 @@
+const env = process.env.REACT_APP_NODE_ENV || 'development';
+
+const defaults = {
+  website: 'https://www.rumblum.com',
+};
+
+const environment = require(`./${env}`);
+
+export default Object.assign(defaults, environment);
+
