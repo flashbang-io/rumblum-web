3ad80b5e51b08658e409284597a4b46afe8fe21b
Jack Scott <jack.rob.scott@gmail.com>
2018-01-21 17:00:41 +0800

    created pending invitation screen #56

--- src/shared/containers/App.js
+++ src/shared/containers/App.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { compose } from 'redux';
 import { Switch, Route, Redirect, withRouter } from 'react-router-dom';
-import { attemptCheckPlayer } from '../../player/player.reducer';
+import { attemptCheckPlayer, attemptLogoutPlayer } from '../../player/player.reducer';
 import config from '../../config';
 import LoginPage from '../../player/containers/LoginPage';
 import RegisterPage from '../../player/containers/RegisterPage';
@@ -14,6 +14,7 @@ import Frame from './Frame';
 import Splash from '../components/Splash';
 import Preview from './Preview';
 import Helpers from './Helpers';
+import Pending from '../components/Pending';
 
 class App extends Component {
 
@@ -30,10 +31,22 @@ class App extends Component {
     }
   }
 
+  handleLogout() {
+    this.props.attemptLogoutPlayer();
+  }
+
   render() {
-    const { checked } = this.props;
+    const { checked, player } = this.props;
     if (!checked) {
       return <Splash />;
+    }
+    if (player && player.pending) {
+      return (
+        <Pending
+          handleLogout={ () => this.handleLogout() }
+          { ...this.props }
+        />
+      );
     }
     return (
       <div>
@@ -55,14 +68,22 @@ class App extends Component {
 
 App.propTypes = {
   attemptCheckPlayer: PropTypes.func.isRequired,
+  attemptLogoutPlayer: PropTypes.func.isRequired,
   checked: PropTypes.bool.isRequired,
+  player: PropTypes.shape({
+    pending: PropTypes.bool,
+  }),
   location: PropTypes.shape({
     pathname: PropTypes.string.isRequired,
   }).isRequired,
 };
 
-const mapStateToProps = ({ player: { checked } }) => ({ checked });
-const mapDispatchToProps = { attemptCheckPlayer };
+App.defaultProps = {
+  player: null,
+}
+
+const mapStateToProps = ({ player: { checked, current, loading } }) => ({ checked, player: current, loading });
+const mapDispatchToProps = { attemptCheckPlayer, attemptLogoutPlayer };
 export default compose(
   withRouter,
   connect(mapStateToProps, mapDispatchToProps),
