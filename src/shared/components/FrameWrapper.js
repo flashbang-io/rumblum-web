45cabd333373e148430f5428fc79affa0dd60a2b
Jack Scott <jack.rob.scott@gmail.com>
2018-01-28 19:11:57 +0800

    align to top with spacing

--- src/shared/components/FrameWrapper.js
+++ src/shared/components/FrameWrapper.js
@@ -3,10 +3,10 @@ import styled from 'styled-components';
 export default styled.div`
   background-color: ${props => props.theme.colors.off};
   min-height: 100vh;
-  padding: 30px 0;
+  padding: 80px 0;
   box-sizing: border-box;
   display: flex;
   flex-direction: column;
   align-items: center;
-  justify-content: center;
+  justify-content: flex-start;
 `;
