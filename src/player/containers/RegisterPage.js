5979e4801a8f2340f590e854073a4642fe7c1def
Jack Scott <mast.j.scott@gmail.com>
2018-01-30 09:39:33 +0800

    build in prefill values for register page

--- src/player/containers/RegisterPage.js
+++ src/player/containers/RegisterPage.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 import { compose } from 'redux';
 import { connect } from 'react-redux';
 import { withRouter } from 'react-router-dom';
+import queryString from 'query-string';
 import DocumentTitle from 'react-document-title';
 import { redirectAuthenticatedGuard } from '../../guards';
 import { attemptCreatePlayer, cleanPlayer } from '../player.reducer';
@@ -11,6 +12,18 @@ import RegisterForm from './RegisterForm';
 import Page from '../components/Page';
 
 class RegisterPage extends Component {
+
+  constructor(props) {
+    super(props);
+    const { email, first, last } = queryString.parse(props.location.search);
+    this.state = {
+      init: {
+        email,
+        firstName: first,
+        lastName: last,
+      },
+    };
+  }
 
   componentWillUnmount() {
     this.props.cleanPlayer();
@@ -30,6 +43,7 @@ class RegisterPage extends Component {
             <Title>Sign Up</Title>
             <RegisterForm
               handleSubmit={ event => this.handleSubmit(event) }
+              initialValues={ this.state.init }
               { ...this.props }
             />
             <Button small="true" dull="true" to="/login">Login</Button>
@@ -44,6 +58,9 @@ class RegisterPage extends Component {
 RegisterPage.propTypes = {
   attemptCreatePlayer: PropTypes.func.isRequired,
   cleanPlayer: PropTypes.func.isRequired,
+  location: PropTypes.shape({
+    search: PropTypes.string.isRequired,
+  }).isRequired,
   history: PropTypes.shape({
     push: PropTypes.func.isRequired,
   }).isRequired,
